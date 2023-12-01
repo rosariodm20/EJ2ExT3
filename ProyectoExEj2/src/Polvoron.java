a3c359fbaf6acaf8f7a3d4a1ddb21bc96b95db60
rosariodm20 <rosariodelgadomoreno17@gmail.com>
2023-12-01 19:44:02 +0100

    Soy Rosario Delgado Moreno y me encantan lospolvorones

--- ProyectoExEj2/src/Polvoron.java
+++ ProyectoExEj2/src/Polvoron.java
@@ -3,7 +3,7 @@ public class Polvoron {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Soy Rosario Delgado Moreno y me encantan lospolvorones");
 	}
 
 }
