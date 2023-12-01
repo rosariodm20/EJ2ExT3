67b0756c6adc2a557f7f91b5017a5fe4934c9c46
rosariodm20 <rosariodelgadomoreno17@gmail.com>
2023-12-01 19:50:17 +0100

    Pero mi preferido es el de chocolate.

--- ProyectoExEj2/src/Turron.java
+++ ProyectoExEj2/src/Turron.java
@@ -4,7 +4,7 @@ public class Turron {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Soy Rosario Delgado Moreno y meencantan los turrones.");
-System.out.println("Pero mi preferido es el de chocolate");
+System.out.println("Pero mi preferido es el de chocolate.");
 	}
 
 }
