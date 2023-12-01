01a7a5b3107243a4c498e843e047f2b2b1494a1d
rosariodm20 <rosariodelgadomoreno17@gmail.com>
2023-12-01 19:43:20 +0100

    Soy Rosario Delgado Moreno y meencantan los turrones

---
+++ ProyectoExEj2/src/Turron.java
@@ -0,0 +1,9 @@
+
+public class Turron {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Soy Rosario Delgado Moreno y meencantan los turrones");
+	}
+
+}
